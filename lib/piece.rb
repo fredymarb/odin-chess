f4efd5463dd30e66de0affd3dd963d6cf9e0c3c6
fredymarb <29468132+fredymarb@users.noreply.github.com>
2024-10-03 19:12:12 +0000

    feat: Piece#enemy?(notation)
    desc: checks if the piece at  passed arg is
      an enemy of the current piece

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,7 +1,13 @@
 class Piece
+  attr_reader :color
+
   def initialize(board, location, color)
     @board = board
     @location = location
     @color = color
+  end
+
+  def enemy?(notation)
+    !@board.empty?(notation) && @board[notation].color != @color
   end
 end
